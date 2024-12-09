dadd9c99b4f38a00837cc223041482936fe40583
Mateusz Bogusław Pruszyński <86022@student.pb.edu.pl>
2024-12-09 22:52:10 +0100

    init

---
+++ BlackJackWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackJackWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
